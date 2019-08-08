6ad9d2478aeebd91274c60fe9caf022e297eecd9
Ryan McKenna <ryan@elixirsoftware.co.uk>
2019-08-08 15:52:01 +0100

    Implementation Tuples, vectors and points

--- features/step_definitions/tuples.steps.ts
+++ features/step_definitions/tuples.steps.ts
@@ -1,100 +1,119 @@
 import { loadFeature, defineFeature} from 'jest-cucumber';
-//mport assert = require('assert');
+import { ITuple, Tuple, equalTuples, Point, Vector } from '../../src/functions/utils';
 
-const feature = loadFeature('../some_features/tuples.feature', {loadRelativePath: true});
+const feature = loadFeature('../tuples/tuples.feature', {loadRelativePath: true});
 
 defineFeature(feature, (test) => {
 
-    test('A tuple with w=1.0 is a point', ({ given, when, then, and }) => {
+    test('A tuple with w = 1.0 is a point', ({ given, when, then, and }) => {
 
-    	given(/^A tuple a (.*)$/, (arg0, table) => {
-            pending();
+		let tupleA: ITuple;
+		let tupleAx: number;
+		let tupleAy: number;
+		let tupleAz: number;
+		let tupleAw: number;
+
+
+		let regex = '[-0-9., ]+';
+
+    	given(/^A tuple a = (.*)$/, (arg0, table) => {
+
+			let temp = table[0].Value.match(regex)[0];
+			let stringArr = temp.split(',');
+			let numArr = [0,0,0,0];
+
+			for (let index = 0; index < stringArr.length; index++ ){
+				numArr[index] = parseFloat(stringArr[index]);
+			}
+
+			tupleA = new Tuple(numArr[0],numArr[1],numArr[2],numArr[3]);
     	});
 
     	when('When I access the properties of a', () => {
-            pending();
+			tupleAx = tupleA.x;
+			tupleAy = tupleA.y;
+			tupleAz = tupleA.z;
+			tupleAw = tupleA.w;
     	});
 
-    	then(/^a.x = (.*)$/, (arg0) => {
-            pending();
+    	then(/^a.x = (.*)$/, (xprop) => {
+            expect(tupleAx).toEqual(parseFloat(xprop));
     	});
 
-    	and(/^a.y = (.*)$/, (arg0) => {
-            pending();
+    	and(/^a.y = (.*)$/, (yprop) => {
+            expect(tupleAy).toEqual(parseFloat(yprop));
     	});
 
-    	and(/^a.z = (.*)$/, (arg0) => {
-            pending();
+    	and(/^a.z = (.*)$/, (zprop) => {
+            expect(tupleAz).toEqual(parseFloat(zprop));
     	});
 
-    	and(/^a.w = (.*)$/, (arg0) => {
-            pending();
+    	and(/^a.w = (.*)$/, (wprop) => {
+            expect(tupleAw).toEqual(parseFloat(wprop));
     	});
 
     	and('a is a point', () => {
-            pending();
+			const pointA: ITuple = new Point(tupleAx,tupleAy,tupleAz);
+
+			expect(equalTuples(tupleA,pointA)).toBe(true);
     	});
 
     	and('a is not a vector', () => {
-            pending();
-    	});
-    });
+			const vectorA: ITuple =  new Vector(tupleAx,tupleAy,tupleAz);
 
-    test('A tuple with w=0 is a vector', ({ given, when, then, and }) => {
-
-    	given(/^A tuple a (.*)$/, (arg0, table) => {
-            pending();
-    	});
-
-    	when('When I access the properties of a', () => {
-            pending();
-    	});
-
-    	then(/^a.x = (.*)$/, (arg0) => {
-            pending();
-    	});
-
-    	and(/^a.y = (.*)$/, (arg0) => {
-            pending();
-    	});
-
-    	and(/^a.z = (.*)$/, (arg0) => {
-            pending();
-    	});
-
-    	and(/^a.w = (.*)$/, (arg0) => {
-            pending();
-    	});
-
-    	and('a is not a point', () => {
-            pending();
-    	});
-
-    	and('a is a vector', () => {
-            pending();
-    	});
-    });
-
-   test('point() creates tuples with w=1', ({ given, then }) => {
-
-    	given(/^p ← point\((.*), -(.*), (.*)\)$/, (arg0, arg1, arg2) => {
-            pending();
-    	});
-
-    	then(/^p is equal to tuple\((.*), -(.*), (.*), (.*)\)$/, (arg0, arg1, arg2, arg3) => {
-            pending();
+			expect(equalTuples(tupleA,vectorA)).toBe(false);
     	});
     });
 
 
     test('vector() creates tuples with w=0', ({ given, then }) => {
 
-    	given(/^v ← vector\((.*), -(.*), (.*)\)$/, (arg0, arg1, arg2) => {
-            pending();
-    	});
+		let regex_int = '[-0-9]+';
+		let mockVec: ITuple;
+    	given(/^v = vector\((.*), (-.*), (.*)\)$/, (arg0, arg1, arg2) => {
+			let vecX = parseInt(arg0.match(regex_int)[0]);
+			let vecY = parseInt(arg1.match(regex_int)[0]);
+			let vecZ = parseInt(arg2.match(regex_int)[0]);
 
-    	then(/^v is equal to tuple\((.*), -(.*), (.*), (.*)\)$/, (arg0, arg1, arg2, arg3) => {
-            pending();
+			mockVec = new Vector(vecX, vecY, vecZ);
+		});
+
+    	then(/^v is equal to tuple\((.*), (-.*), (.*), (.*)\)$/, (arg0, arg1, arg2, arg3) => {
+			let tupX = parseInt(arg0.match(regex_int)[0]);
+			let tupY = parseInt(arg1.match(regex_int)[0]);
+			let tupZ = parseInt(arg2.match(regex_int)[0]);
+			let tupW = parseInt(arg3.match(regex_int)[0]);
+
+			let mockTuple: ITuple = new Tuple(tupX, tupY,tupZ,tupW);
+
+			expect(mockTuple.w).toEqual(0);
+			expect(equalTuples(mockVec,mockTuple)).toBe(true);
+    	});
+	});
+
+	test('point() creates tuples with w=1', ({ given, then }) => {
+
+		let regex_int = '[-0-9]+';
+		let mockPoint: ITuple;
+
+		given(/^p = point\((.*), -(.*), (.*)\)$/, (arg0, arg1, arg2) => {
+			let pointX = parseInt(arg0.match(regex_int)[0]);
+			let pointY = parseInt(arg1.match(regex_int)[0]);
+			let pointZ = parseInt(arg2.match(regex_int)[0]);
+
+			mockPoint = new Point(pointX, pointY, pointZ);
+		});
+
+    	then(/^p is equal to tuple\((.*), -(.*), (.*), (.*)\)$/, (arg0, arg1, arg2, arg3) => {
+			let tupX = parseInt(arg0.match(regex_int)[0]);
+			let tupY = parseInt(arg1.match(regex_int)[0]);
+			let tupZ = parseInt(arg2.match(regex_int)[0]);
+			let tupW = parseInt(arg3.match(regex_int)[0]);
+
+			let mockTuple: ITuple = new Tuple(tupX, tupY,tupZ,tupW);
+
+			expect(mockPoint.w).toEqual(1);
+			expect(equalTuples(mockPoint,mockTuple)).toBe(true);
     	});
     });
 });
--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,4 +1,4 @@
-export function equal(a, b){
+export function equal(a: number, b: number){
 
     let EPSILON = 0.00001;
     let diff = a -b;
@@ -14,7 +14,7 @@ export function equal(a, b){
 
 }
 
-export function equalTuples(a, b){
+export function equalTuples(a: ITuple, b: ITuple){
 
     if(
         equal(a.x,b.x) &&
@@ -27,4 +27,52 @@ export function equalTuples(a, b){
         return false;
     }
 
+}
+
+export interface ITuple {
+    x: number;
+    y: number;
+    z: number;
+    w: number;
+}
+
+export class Tuple implements ITuple {
+    constructor(X0: number,Y0: number,Z0 :number, W0: number){
+        this.x = X0;
+        this.y = Y0;
+        this.z = Z0;
+        this.w = W0;
+    }
+
+    public x: number;
+    public y: number;
+    public z: number;
+    public w: number;
+}
+
+
+export class Point implements ITuple{
+    constructor(X0: number, Y0: number, Z0: number){
+        this.x = X0;
+        this.y = Y0;
+        this.z = Z0;
+        this.w = 1;
+    }
+    public x: number;
+    public y: number;
+    public z: number;
+    public w: number;
+}
+
+export class Vector implements ITuple{
+    constructor(X0: number, Y0: number, Z0: number){
+        this.x = X0;
+        this.y = Y0;
+        this.z = Z0;
+        this.w = 0;
+    }
+    public x: number;
+    public y: number;
+    public z: number;
+    public w: number;
 }
